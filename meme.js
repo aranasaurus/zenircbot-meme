fbcba07a5905d22ce82c00755af0c161fb98ea3f
Aaron Parecki <aaron@parecki.com>
2013-02-13 12:04:26 -0800

    Match memes more aggressively in regular sentences if the patterns match.

--- meme.js
+++ meme.js
@@ -10,28 +10,40 @@ zen.register_commands('meme.js', [{
         'after the message. The default image is the aliens guy.'
 }]);
 
-var getMeme = function(msg, channel) {
-    var t1, t2, img, url;
+var getMeme = function(msg, channel, use_default) {
+    var t1, t2, url;
+    var img = false;
     var message = msg;
 
     // grab the image url (or figure it out)
-    if (message.indexOf('[') > 0) {
-        var start = message.indexOf('[');
-        var end = message.indexOf(']');
-        img = message.substring(start+1, end);
-        message = message.substring(0, start);
-    } else if (message.match(/^I don't always.*/i)) {
+    if (match=message.match(/(.+) \[?(http:\/\/[^\]]+)$/)) {
+        message = match[1];
+        img = match[2];
+    } else if (message.match(/^i don't always.*/i)) {
         img = 'http://memecaptain.com/most_interesting.jpg';
-    } else if (message.match(/.*ALL THE .*$/i)) {
+    } else if (match=message.match(/([^ ]+ all the .*s)[!\.]?$/i)) {
+        message = match[1];
         img = 'http://memecaptain.com/all_the_things.jpg';
-    } else if (message.match(/^one does not simply .*$/i)) {
+    } else if (message.match(/^one does not simply .+$/i)) {
         img = 'http://memecaptain.com/boromir.jpg';
-    } else if (message.match(/^not sure if .* or .*$/i)) {
+    } else if (message.match(/^not sure if .+ or .+$/i)) {
         img = 'http://memecaptain.com/fry.png';
-    } else if (message.match(/^.*I hea?rd you like .*$/i)) {
+    } else if (message.match(/^.*I hea?rd you like .+$/i)) {
         img = 'http://memecaptain.com/xzibit.jpg';
-    } else {
+    } else if (message.match(/.+ so hot right now$/i)) {
+        img = 'http://cdn.buzznet.com/assets/users16/rich/default/mugatu--large-msg-124777042649.jpg';
+    } else if(use_default == true) {
         img = 'http://memecaptain.com/aliens.jpg';
+    }
+
+    if(img == false) {
+        // Only make a meme if the image is set. 
+        // This function is called for all IRC messages. if use_default=true,
+        // then img will be set to the default meme. This is only the case when 
+        // called explicitly from the !meme command. Otherwise, it is called
+        // with use_default=false and we should not say a meme unless one of the
+        // patterns matches.
+        return;
     }
 
     // split the message into top/bottom
@@ -83,11 +95,15 @@ sub.on('message', function( channel, message ) {
     var msg = JSON.parse(message);
     if (msg.version == 1) {
         if (msg.type == 'privmsg') {
-            if (/^!meme/i.test(msg.data.message)) {
-                getMeme(msg.data.message.substring(6, msg.data.message.length), msg.data.channel);
+            if (/^!meme /i.test(msg.data.message)) {
+                console.log("explicit: "+msg.data.message);
+                getMeme(msg.data.message.substring(6, msg.data.message.length), msg.data.channel, true);
+            } else {
+                console.log("implicit: "+msg.data.message);
+                getMeme(msg.data.message, msg.data.channel, false);
             }
         } else if (msg.type == 'topic') {
-            getMeme(msg.data.topic, msg.data.channel);
+            getMeme(msg.data.topic, msg.data.channel, true);
         }
     }
 });
