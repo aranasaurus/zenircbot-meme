c7bead17652535dbb6e7fa9e6b3062e950cd9405
Ryan Arana <ryan.arana@gmail.com>
2013-02-12 20:19:56 -0800

    Add some meme detection regexes and make elipses work as a splitter for top/bottom

--- meme.js
+++ meme.js
@@ -14,23 +14,43 @@ var getMeme = function(msg, channel) {
     var t1, t2, img, url;
     var message = msg;
 
+    // grab the image url (or figure it out)
     if (message.indexOf('[') > 0) {
         var start = message.indexOf('[');
         var end = message.indexOf(']');
         img = message.substring(start+1, end);
         message = message.substring(0, start);
+    } else if (message.match(/^I don't always.*/i)) {
+        img = 'http://memecaptain.com/most_interesting.jpg';
+    } else if (message.match(/.*ALL THE .*$/i)) {
+        img = 'http://memecaptain.com/all_the_things.jpg';
+    } else if (message.match(/^one does not simply .*$/i)) {
+        img = 'http://memecaptain.com/boromir.jpg';
+    } else if (message.match(/^not sure if .* or .*$/i)) {
+        img = 'http://memecaptain.com/fry.png';
+    } else if (message.match(/^.*I hea?rd you like .*$/i)) {
+        img = 'http://memecaptain.com/xzibit.jpg';
     } else {
         img = 'http://memecaptain.com/aliens.jpg';
     }
 
+    // split the message into top/bottom
     if (message.indexOf('.') > 0) {
         var dot = message.indexOf('.');
         t1 = message.substring(0, dot+1);
         t2 = message.substring(dot+1, message.length);
+
+        if (t2[0] === '.') {
+            while (t2[0] === '.') {
+                t2 = t2.substring(1, t2.length);
+            }
+            t1 = t1 + '..';
+        }
     } else {
         t1 = '';
         t2 = message;
     }
+
     console.log('sending request t1='+t1+' t2='+t2+'img='+img);
     var options = {
         host: 'memecaptain.com',
