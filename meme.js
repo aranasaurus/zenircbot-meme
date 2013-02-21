12949bc8b6f26dccf81440fe66925733a5aa3009
Aaron Parecki <aaron@parecki.com>
2013-02-20 19:32:17 -0800

    Moves meme detectors into their own file

--- meme.js
+++ meme.js
@@ -4,6 +4,7 @@ var sub = zen.get_redis_client();
 var http = require('http');
 var request = require('request');
 var querystring = require('querystring');
+var memeDetectors = require('./memes.js').detectors;
 
 var DEBUG = false;
 var debugLog = function(msg) {
@@ -114,97 +115,6 @@ var getMeme = function(msg, channel, explicit) {
 
     debugLog('getMeme called: msg: "' + msg + '" channel: ' + channel + ' explicit: ' + explicit);
 
-    var detectors = [
-        {
-            regex: /(^I don't always .+) (but when I do,? .+$)/i,
-            img: 'http://memecaptain.com/most_interesting.jpg',
-            testString: "I don't always test my code, but when I do, I do it in production."
-        },
-        {
-            regex: /([^ ]+ all the .*s)()/i,
-            img: 'http://memecaptain.com/all_the_things.jpg',
-            testString: "Refactor all the things!"
-        },
-        {
-            regex: /(^one does not simply) (.+$)/i,
-            img: 'http://memecaptain.com/boromir.jpg',
-            testString: "One does not simply meme in IRC."
-        },
-        {
-            regex: /(^not sure if .+) (or .+$)/i,
-            img: 'http://memecaptain.com/fry.jpg',
-            testString: "Not sure if this is working, or is awesome."
-        },
-        {
-            regex: /(^yo,? (?:dawg|dog)[\.,]* I hea?rd (?:you|u) like .+) (so (?:i|we) .+$)/i,
-            img: 'http://memecaptain.com/xzibit.jpg',
-            testString: "Yo dawg I herd you like regex. so we put a regex in yo regex so you can match while you match."
-        },
-        {
-            regex: /(^the .+) (is too (?:damn )?high[!\.]?$)/i,
-            img: 'http://memecaptain.com/too_damn_high.jpg',
-            testString: "The amount of memes in this channel is too damn high!"
-        },
-        {
-            regex: /(^(?:brace yourselves,? )?)(.+ (?:is|are) coming[!\.]?$)/i,
-            img: 'http://memecaptain.com/ned_stark.jpg',
-            testString: "Brace yourselves the memes are coming."
-        },
-        {
-            regex: /(^what if) (.+[\?!]+$)/i,
-            img: 'http://memecaptain.com/conspiracy_keanu.jpg',
-            testString: "What if space nachos?!"
-        },
-        {
-            regex: /(^.+) (and you should feel bad\.?$)/i,
-            img: 'http://memecaptain.com/you_should_feel_bad.jpg',
-            testString: "This meme is bad; and you should feel bad."
-        },
-        {
-            regex: /(^.+) (y u no .+$)/i,
-            img: 'http://memecaptain.com/y_u_no.jpg',
-            testString: "IRC y u no do memes yourself?"
-        },
-        {
-            regex: /(^am i the only one around here) (.+$)/i,
-            img: 'http://memecaptain.com/walter.jpg',
-            testString: "Am I the only one around here who gives a shit about the rules?!"
-        },
-        {
-            regex: /(^.+) (so hot right now[\.!]*$)/i,
-            img: 'http://cdn.buzznet.com/assets/users16/rich/default/mugatu--large-msg-124777042649.jpg',
-            testString: "Memes, so hot right now."
-        },
-        {
-            regex: /(^ermahgerd[!\.]*) (.+$)/i,
-            img: 'http://i.imgur.com/KGaxT49.jpg',
-            testString: "ermahgerd! Space nachos!"
-        },
-        {
-            regex: /(^.+) (is strong with this one[!\.]*$)/i,
-            img: 'http://twimg0-a.akamaihd.net/profile_images/1244937644/02emperor350.jpg',
-            testString: "The force is strong with this one."
-        },
-        {
-            regex: /(^this is) (.+!$)/i,
-            img: 'http://itechnow.com/wp-content/uploads/2013/01/This-is-Sparta-.jpg',
-            testString: "This is IRC!"
-        },
-        {
-            regex: /(.+ over )([0-9]+)([\.!]*)$/i,
-            img: 'http://cache.ohinternet.com/images/thumb/1/1e/Over_9000_Vector_by_Vernacular.jpg/618px-Over_9000_Vector_by_Vernacular.jpg',
-            testString: "Something over 5", // TODO: Get a valid testString for this one (I'm unfamiliar)
-            postProc: function(match) {
-                if (parseInt(match[2]) < 1000) {
-                    match[2] *= 1000;
-                }
-                message = match[1] + match[2] + match[3];
-                t1 = false;
-                t2 = false;
-            }
-        }
-    ];
-
     if (explicit && ((match=message.match(/(.+) \[?(https?:\/\/[^\]]+)\]?$/)) || (match=message.match(/(.+) \[([^\]]+)\]$/)))) {
         debugLog('explicit message detected, skipping detectors.');
         message = match[1];
@@ -217,8 +127,8 @@ var getMeme = function(msg, channel, explicit) {
             return;
         }
     } else {
-        for (var d in detectors) {
-            var detector = detectors[d];
+        for (var d in memeDetectors) {
+            var detector = memeDetectors[d];
             debugLog('checking detector: ' + detector.testString);
             var match = detector.regex.exec(message);
             if (match) {
