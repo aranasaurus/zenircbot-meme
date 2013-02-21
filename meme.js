9b5b86aa3752503082608ea8f98450eec3b4f928
Aaron Parecki <aaron@parecki.com>
2013-02-20 18:42:10 -0800

    Use auto-detected image on explicit !meme command. Now supports `!meme thing [image search]` syntax!

--- meme.js
+++ meme.js
@@ -2,6 +2,8 @@ var ZenIRCBot = require('zenircbot-api').ZenIRCBot;
 var zen = new ZenIRCBot();
 var sub = zen.get_redis_client();
 var http = require('http');
+var request = require('request');
+var querystring = require('querystring');
 
 var DEBUG = false;
 var debugLog = function(msg) {
@@ -18,6 +20,92 @@ zen.register_commands('meme.js', [{
         'half on top, second on bottom. You can override the default image by putting an image url in [] ' +
         'after the message. The default image is the aliens guy.'
 }]);
+
+function imageSearch(query, callback) {
+  query = querystring.stringify(query);
+  debugLog('[image search] '+query);
+  request('http://ajax.googleapis.com/ajax/services/search/images?'+query, function(error, response, body){
+    callback(JSON.parse(body));
+  });
+}
+
+function findSingleImage(search, callback) {
+  var params = {
+    v: '1.0',
+    rsz: '8',
+    q: search,
+    safe: 'active'
+  };
+
+  imageSearch(params, function(data){
+    var images = data.responseData.results;
+    if(images.length > 0) {
+      images.sort(function() {return 0.5 - Math.random()});
+      var image = images[0];
+      callback(image.unescapedUrl);
+    }
+  });
+}
+
+var sendMeme = function(channel, img, message, t1, t2) {
+    debugLog('img: ' + img);
+    debugLog('t1: ' + t1);
+    debugLog('t2: ' + t2);
+
+    if (img == false) {
+        debugLog('bailing due to no memes matched in an implicit call');
+        // Only make a meme if the image is set.
+        // This function is called for all IRC messages. If explicit=true,
+        // then img will be set to the default meme. This is only the case when
+        // called explicitly from the !meme command. Otherwise, it is called
+        // with explicit=false and we should not say a meme unless one of the
+        // patterns match.
+        return;
+    }
+
+    // messageRegex splits the text based on ., ..., or |
+    var messageRegex = /(.+[\.+|\|]) ?(.*)/;
+    // split the message into top/bottom only if t1 and t2 are both unset
+    if (!t1 && !t2) {
+        if (messageRegex.test(message)) {
+        var match = messageRegex.exec(message);
+        t1 = match[1].replace('|', '');
+        t2 = match[2];
+        } else {
+            t1 = '';
+            t2 = message;
+        }
+    }
+
+    console.log('sending request t1='+t1+' t2='+t2+' img='+img);
+    var options = {
+        host: 'memecaptain.com',
+        port: 80,
+        path: '/g?u=' + encodeURI(img) + '&t1=' + encodeURI(t1) + '&t2=' + encodeURI(t2),
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    http.get(options, function(res) {
+        var memeData = '';
+        res.setEncoding('utf8');
+
+        res.on('data', function(chunk) {
+            memeData += chunk;
+        });
+
+        res.on('end', function() {
+            if (memeData) {
+                var meme = JSON.parse(memeData);
+                zen.send_privmsg(channel, meme.imageUrl);
+            }
+        });
+    }).on('error', function(e) {
+        // TODO: send this to zenircbot's admin spam, if possible.
+        console.log('got error sending request: ' + e.message);
+    });
+};
 
 var getMeme = function(msg, channel, explicit) {
     var t1 = false, t2 = false, img = false;
@@ -117,10 +205,17 @@ var getMeme = function(msg, channel, explicit) {
         }
     ];
 
-    if (explicit && (match=message.match(/(.+) \[?(https?:\/\/[^\]]+)\]?$/))) {
+    if (explicit && ((match=message.match(/(.+) \[?(https?:\/\/[^\]]+)\]?$/)) || (match=message.match(/(.+) \[([^\]]+)\]$/)))) {
         debugLog('explicit message detected, skipping detectors.');
         message = match[1];
         img = match[2];
+        if(!img.match(/^http/)) {
+            // Find an image and use that!
+            findSingleImage(img, function(imageUrl){
+                sendMeme(channel, imageUrl, message, t1, t2);
+            });
+            return;
+        }
     } else {
         for (var d in detectors) {
             var detector = detectors[d];
@@ -141,68 +236,13 @@ var getMeme = function(msg, channel, explicit) {
         }
 
         // No detectors matched, if this is an explicit call, set img to the default
-        if (explicit) {
+        if (!img && explicit) {
             debugLog('No meme auto detected, using default img: ' + defaultImg);
             img = defaultImg;
         }
     }
-    debugLog('img: ' + img);
-    debugLog('t1: ' + t1);
-    debugLog('t2: ' + t2);
 
-    if (img == false) {
-        debugLog('bailing due to no memes matched in an implicit call');
-        // Only make a meme if the image is set.
-        // This function is called for all IRC messages. If explicit=true,
-        // then img will be set to the default meme. This is only the case when
-        // called explicitly from the !meme command. Otherwise, it is called
-        // with explicit=false and we should not say a meme unless one of the
-        // patterns match.
-        return;
-    }
-
-    // messageRegex splits the text based on ., ..., or |
-    var messageRegex = /(.+[\.+|\|]) ?(.*)/;
-    // split the message into top/bottom only if t1 and t2 are both unset
-    if (!t1 && !t2) {
-        if (messageRegex.test(message)) {
-        var match = messageRegex.exec(message);
-        t1 = match[1].replace('|', '');
-        t2 = match[2];
-        } else {
-            t1 = '';
-            t2 = message;
-        }
-    }
-
-    console.log('sending request t1='+t1+' t2='+t2+' img='+img);
-    var options = {
-        host: 'memecaptain.com',
-        port: 80,
-        path: '/g?u=' + encodeURI(img) + '&t1=' + encodeURI(t1) + '&t2=' + encodeURI(t2),
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    http.get(options, function(res) {
-        var memeData = '';
-        res.setEncoding('utf8');
-
-        res.on('data', function(chunk) {
-            memeData += chunk;
-        });
-
-        res.on('end', function() {
-            if (memeData) {
-                var meme = JSON.parse(memeData);
-                zen.send_privmsg(channel, meme.imageUrl);
-            }
-        });
-    }).on('error', function(e) {
-        // TODO: send this to zenircbot's admin spam, if possible.
-        console.log('got error sending request: ' + e.message);
-    });
+    sendMeme(channel, img, message, t1, t2);
 };
 
 sub.subscribe('in');
