ee32fd35f2c3d117b5d04821398c3048ad295d36
Aaron Parecki <aaron@parecki.com>
2013-02-20 23:16:54 -0800

    store files in "m" folder instead

--- api/app.rb
+++ api/app.rb
@@ -8,9 +8,9 @@ get '/g' do
   open(params[:u], 'rb') do |f|
     i = MemeCaptain.meme_top_bottom(f, params[:t1], params[:t2], :font => './Impact.ttf')
     filename = "#{DateTime.now.to_sxg}#{SecureRandom.random_number(60**5).to_sxg}.jpg"
-    i.write("./public/memes/#{filename}")
+    i.write("./public/m/#{filename}")
     headers 'Content-type' => 'application/json'
-    {imageUrl: "http://#{request.host}/memes/#{filename}"}.to_json
+    {imageUrl: "http://#{request.host}/m/#{filename}"}.to_json
   end
 
 end
